9c56a82f3153b4930fd51318638b1e730b4f2477
Jérémie Koster <jkoster@quantmetry.com>
2024-06-04 08:48:50 +0200

    fix: remove main block

--- lib/data/json/json_loader.dart
+++ lib/data/json/json_loader.dart
@@ -4,14 +4,6 @@ import 'package:flutter/services.dart' show rootBundle;
 
 import '../../models/result.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  String input = await rootBundle.loadString("assets/result.json");
-  Map<String, dynamic> data_decoded = jsonDecode(input);
-  print("Loser score is : ${data_decoded['loser_score']}");
-  print("Loser is : ${data_decoded['loser_id']}");
-}
-
 Future<List<Result>> loadResults() async {
   final String jsonString = await rootBundle.loadString("assets/result.json");
   final List<dynamic> jsonResponse = jsonDecode(jsonString);
